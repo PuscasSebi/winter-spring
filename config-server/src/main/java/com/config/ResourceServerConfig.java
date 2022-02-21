1376fb5df88670d6e907ccf1cfc6b3a6c27e72fe
Sebi Puscas <spuscas@grubhub.com>
2022-02-22 00:42:25 +0200

    authorization with token works, need to work with basic auth now.. idk how other services are going to start up..

--- config-server/src/main/java/com/config/ResourceServerConfig.java
+++ config-server/src/main/java/com/config/ResourceServerConfig.java
@@ -1,9 +1,8 @@
-package config.config;
+package com.config;
 
 
 import authentication.JwtOpaqueIssuerAuthenticationManagerResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
